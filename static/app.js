c0d8a6a8d7e48cdd2d9916725285970ce048c270
javierj <javierj@ilitia.com>
2024-05-24 20:45:32 +0200

    design change

--- static/app.js
+++ static/app.js
@@ -50,3 +50,51 @@ function toggleSendButton() {
     sendButton.style.display = "block";
   }
 }
+
+function getRandomPosition(element) {
+  const container = document.getElementById("container");
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+  const elementWidth = element.clientWidth;
+  const elementHeight = element.clientHeight;
+
+  // Ajustar para que no se salga del contenedor
+  const maxX = containerWidth - elementWidth;
+  const maxY = containerHeight - elementHeight;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  return { x: randomX, y: randomY };
+}
+
+function toggleImage(id) {
+  debugger;
+  const image = document.getElementById(id);
+
+  if (image.style.display === "none" || image.style.display === "") {
+    const position = getRandomPosition(image);
+    image.style.left = `${position.x}px`;
+    image.style.top = `${position.y}px`;
+    image.style.display = "block";
+    image.style.opacity = 1;
+  } else {
+    image.style.opacity = 0;
+    setTimeout(() => {
+      image.style.display = "none";
+    }, 10); // Espera a que termine la transición de opacidad
+  }
+}
+
+setInterval(() => toggleImage("randomImage1"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage2"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage3"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage4"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage5"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage6"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage7"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage8"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage9"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage10"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage11"), Math.random() * 5000);
+setInterval(() => toggleImage("randomImage12"), Math.random() * 5000);
