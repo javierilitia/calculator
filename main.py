f84d6c7fa08f331434493fa1e8144fbcb2d20674
javierj <javierj@ilitia.com>
2024-05-27 17:47:01 +0200

    plain chat

--- main.py
+++ main.py
@@ -1,67 +1,89 @@
-﻿from fastapi import FastAPI, WebSocket
-
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
+﻿from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Montar la carpeta static
-app.mount("/static", StaticFiles(directory="static"), name="static")
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            const client_id = prompt("Enter your nickname");
+            document.querySelector("#ws-id").textContent = client_id;
+            const ws = new WebSocket(`wss://calculator-api-python.azurewebsites.net/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-# Configurar Jinja2Templates
-templates = Jinja2Templates(directory="templates")
-from typing import List
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime
-import json
-# Dictionary to store connected WebSocket clients
-connected_users = {}
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # can alter with time
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+        self.users: list[str] = []
+
+    async def connect(self, websocket: WebSocket, client_id: str):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        self.users.append(client_id)
+        await self.send_clients_list()
+
+    async def disconnect(self, websocket: WebSocket, client_id: str):
+        self.active_connections.remove(websocket)
+        self.users.remove(client_id)
+        await self.send_clients_list()
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+    async def send_clients_list(self):
+        clients_list = ",".join(map(str, self.users))
+        for connection in self.active_connections:
+            await connection.send_text(f"Connected clients: {clients_list}")
+
+
+manager = ConnectionManager()
+
 
 @app.get("/")
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "title": "Home Page"})
-
-async def notify_users():
-    user_list = list(connected_users.keys())
-    message = json.dumps({"type": "user_list", "users": user_list})
-    for user_ws in connected_users.values():
-        await user_ws.send_text(message)
+async def get():
+    return HTMLResponse(html)
 
 
-@app.websocket("/ws/{user_id}")
-async def websocket_endpoint(user_id: str, websocket: WebSocket):
-    await websocket.accept()
-
-    # Store the WebSocket connection in the dictionary
-    connected_users[user_id] = websocket
-    await notify_users()
-
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id)
     try:
         while True:
             data = await websocket.receive_text()
-            # Send the received data to the other user
-            message = json.dumps({"user": user_id, "data": data})
-            for user, user_ws in connected_users.items():
-                if user != user_id:
-                    await user_ws.send_text(message)
-    except:
-        # If a user disconnects, remove them from the dictionary
-        del connected_users[user_id]
-        await websocket.close()
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+            await manager.broadcast(f"{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, client_id)
+        await manager.broadcast(f"{client_id} left the chat")
